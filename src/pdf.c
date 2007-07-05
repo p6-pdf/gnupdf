4c2f9815721d10838d91141f4f6e19b25fbd09f5
jemarch <>
2007-07-05 21:20:15 +0000

    new file

---
+++ src/pdf.c
@@ -0,0 +1,32 @@
+/* -*- mode: C -*- Time-stamp: "07/07/05 21:11:27 jemarch"
+ *
+ *       File:         pdf.c
+ *       Author:       Jose E. Marchesi (jemarch@gnu.org)
+ *       Date:         Thu Jul  5 21:07:10 2007
+ *
+ *       GNU PDF Library - Main implementation file
+ *
+ */
+
+/* Copyright (C) 2007 Jose E. Marchesi */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+char* gnupdf_version = "1.3";
+
+
+
+/* End of pdf.c */
