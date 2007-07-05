fffe1c6f7d09b84c4a025924bfcaa3c3b8767dca
jemarch <>
2007-07-05 20:17:12 +0000

    New files

---
+++ src/pdf.h
@@ -0,0 +1,34 @@
+/* -*- mode: C -*- Time-stamp: "07/07/05 19:58:11 jemarch"
+ *
+ *       File:         pdf.h
+ *       Author:       Jose E. Marchesi (jemarch@gnu.org)
+ *       Date:         Thu Jul  5 19:52:48 2007
+ *
+ *       GNU PDF Library - Main header file
+ *
+ */
+
+/* Copyright (C) 2007 Jose E. Marchesi */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _PDF_H
+#define _PDF_H
+
+
+
+
+
+#endif /* pdf.h */
