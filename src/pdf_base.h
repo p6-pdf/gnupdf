283c5979b872bf7b482b97b7c5bbce98157029b6
jemarch <>
2007-07-06 20:35:14 +0000

    new file

---
+++ src/pdf_base.h
@@ -0,0 +1,38 @@
+/* -*- mode: C -*- Time-stamp: "07/07/06 20:23:26 jemarch"
+ *
+ *       File:         pdf_base.h
+ *       Author:       Jose E. Marchesi (jemarch@gnu.org)
+ *       Date:         Fri Jul  6 20:22:05 2007
+ *
+ *       GNU PDF Library - Common resources for the library modules
+ *
+ */
+
+/* Copyright (C) 2007 Jose E. Marchesi */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* This library module implement the abstract input/output routines
+   used by the pdf lexical scanner */
+
+#ifndef _PDF_BASE_H
+#define _PDF_BASE_H
+
+#define PDF_OK 0
+#define PDF_ERROR 1
+
+#endif /* pdf_base.h */
+
+/* End of pdf_base.h */
