7a89ed635a19e535460b8b0bd86aaa78abb87fcd
jemarch <>
2007-07-10 18:52:34 +0000

    utilities

---
+++ utils/pdf_filter.c
@@ -0,0 +1,18 @@
+/* -*- mode: C -*- Time-stamp: "07/07/10 18:43:00 jemarch"
+ *
+ *       File:         pdf_filter.c
+ *       Author:       Jose E. Marchesi (jemarch@gnu.org)
+ *       Date:         Tue Jul 10 18:42:07 2007
+ *
+ *       GNU PDF Library - Process input/output with the
+ *                         standard PDF Filters.
+ *
+ */
+
+#include <config.h>
+#include <pdf_filter.h>
+
+
+
+
+/* End of pdf_filter.c */
---
+++ utils/pdf_filter.h
@@ -0,0 +1,21 @@
+/* -*- mode: C -*- Time-stamp: "07/07/10 18:42:48 jemarch"
+ *
+ *       File:         pdf_filter.h
+ *       Author:       Jose E. Marchesi (jemarch@gnu.org)
+ *       Date:         Tue Jul 10 18:40:55 2007
+ *
+ *       GNU PDF Library - Process input/output with the 
+ *                         standard PDF filters.
+ *
+ */
+
+#ifndef PDF_FILTER_H
+#define PDF_FILTER_H
+
+#include <config.h>
+
+
+
+#endif /* pdf_filter.h */
+
+/* End of pdf_filter.h */
