8ef5d6a4c4a86d681531bd4c284bb6c62fcacaa1
jemarch <>
2007-09-15 02:24:16 +0000

    *** empty log message ***

--- utils/pdf_filter.h
+++ utils/pdf_filter.h
@@ -1,4 +1,4 @@
-/* -*- mode: C -*- Time-stamp: "07/07/31 21:39:39 jemarch"
+/* -*- mode: C -*- Time-stamp: "07/09/15 00:05:08 jemarch"
  *
  *       File:         pdf_filter.h
  *       Author:       Jose E. Marchesi (jemarch@gnu.org)
@@ -7,6 +7,23 @@
  *       GNU PDF Library - Process input/output with the 
  *                         standard PDF filters.
  *
+ */
+
+/* Copyright (C) 2007 Jose E. Marchesi */
+/* Copyright (C) 2007 Juan Pedro Bolivar Puente */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef PDF_FILTER_H
