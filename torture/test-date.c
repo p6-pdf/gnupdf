f2e68604cd0ee5e25d9a94f78018b14fb04439db
jemarch <>
2007-10-31 20:23:03 +0000

    *** empty log message ***

--- torture/test-date.c
+++ torture/test-date.c
@@ -1,4 +1,4 @@
-/* -*- mode: C -*- Time-stamp: "07/10/23 12:58:05 jemarch"
+/* -*- mode: C -*- Time-stamp: "07/10/31 20:08:10 jemarch"
  *
  *       File:         test-date.c
  *       Author:       Jose E. Marchesi (jemarch@gnu.org)
@@ -25,7 +25,13 @@
  */
 
 #include <config.h>
-#include <malloc.h>
+
+#ifdef HAVE_MALLOC_H
+ #include <malloc.h>
+#else
+ #include <stdlib.h>
+#endif /* HAVE_MALLOC_H */
+
 #include <xalloc.h>
 #include <string.h>
 #include <stdio.h>
