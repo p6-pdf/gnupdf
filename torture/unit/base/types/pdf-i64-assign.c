b7bc2073b39aabfc95b93779e6acc171658974cb
danividal <>
2008-05-05 15:32:00 +0000

    Adapted comment headers to comply with coding conventions

--- torture/unit/base/types/pdf-i64-assign.c
+++ torture/unit/base/types/pdf-i64-assign.c
@@ -1,3 +1,27 @@
+/* -*- mode: C -*- Time-stamp: ""
+ *
+ *       File:         pdf-i64-assign.c
+ *       Date:         Mon May 05 10:30:01 2008
+ *
+ *       GNU PDF Library - Types Module - pdf_i64_assign test case
+ *
+ */
+
+/* Copyright (C) 2008 Free Software Foundation, Inc. */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <config.h>
 #include <stdio.h>
--- torture/unit/base/types/tsuite-types.c
+++ torture/unit/base/types/tsuite-types.c
@@ -1,3 +1,27 @@
+/* -*- mode: C -*- Time-stamp: ""
+ *
+ *       File:         tsuite-types.c
+ *       Date:         Mon May 05 10:30:01 2008
+ *
+ *       GNU PDF Library - Testcase definition for the types module
+ *
+ */
+
+/* Copyright (C) 2008 Free Software Foundation, Inc. */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <check.h>
 
