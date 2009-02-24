7f783c9b4933c33d7e2b682eaf051fef5df690f8
David Vazquez <davazp@es.gnu.org>
2009-02-24 18:57:41 +0100

    Copyright notices

--- torture/unit/base/fp/pdf-fp-func-4-new.c
+++ torture/unit/base/fp/pdf-fp-func-4-new.c
@@ -1,10 +1,26 @@
-/* -*- mode: C -*- Time-stamp: "2009-02-11 16:07:06 davazp"
+/* -*- mode: C -*- Time-stamp: "2009-02-24 18:57:05 davazp"
  *
  *       File:         pdf-fp-func-eval.c
  *       Date:         Sat Feb  7 18:55:51 2009
  *
  *       GNU PDF Library - Unit tests for pdf_fp_func_4_new
  *
+ */
+
+/* Copyright (C) 2009 Free Software Foundation, Inc. */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <config.h>
