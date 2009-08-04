08ec12136c6482259d9233838a4605918380601f
Jose E. Marchesi <jemarch@termi>
2009-08-04 02:43:04 +0200

    Revision 708 re-reverted

---
+++ torture/unit/base/fsys/tsuite-fsys.c
@@ -0,0 +1,50 @@
+/* -*- mode: C -*- Time-stamp: "2009-07-27 23:56:43 derhans"
+ *
+ *       File:         tsuite-fsys.c
+ *       Date:         Fri May  1 18:08:12 2009
+ *
+ *       GNU PDF Library - Testcase definition for the Fsys module
+ *
+ */
+
+/* Copyright (C) 2008 Free Software Foundation, Inc. */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <check.h>
+
+extern TCase *test_pdf_fsys_get_free_space (void);
+extern TCase *test_pdf_fsys_get_temp_path_name (void);
+extern TCase *test_pdf_fsys_file_open (void);
+extern TCase *test_pdf_fsys_file_close (void);
+
+
+Suite *
+tsuite_fsys ()
+{
+  Suite *s;
+
+  s = suite_create("fsys");
+  
+  suite_add_tcase (s, test_pdf_fsys_get_free_space ());
+  suite_add_tcase (s, test_pdf_fsys_get_temp_path_name ());
+  suite_add_tcase (s, test_pdf_fsys_file_open ());
+  suite_add_tcase (s, test_pdf_fsys_file_close ());
+
+  return s;
+}
+
+
+/* End of tsuite-fsys.c */
