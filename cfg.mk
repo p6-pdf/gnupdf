181353459f1dbcde2b46f51793ee8c7589310e8c
Jose E. Marchesi <jemarch@termi>
2010-04-19 23:14:16 +0200

    Skip sc_cross_check_PATH_usage_in_tests in make syntax-check.

--- cfg.mk
+++ cfg.mk
@@ -2,4 +2,5 @@ C_SOURCES = $(shell find src -name '*.[chly]')
 local-checks-to-skip = sc_prohibit_strcmp \
                        sc_file_system \
                        sc_trailing_blank \
-                       sc_immutable_NEWS
+                       sc_immutable_NEWS \
+                       sc_cross_check_PATH_usage_in_tests
