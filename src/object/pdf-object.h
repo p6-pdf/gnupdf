9618b0c39525a5799893075e07ca8cb6a2f5db48
Jose E. Marchesi <jemarch@gnu.org>
2011-02-24 00:41:53 +0100

    object: create the layer header file.

---
+++ src/object/pdf-object.h
@@ -0,0 +1,42 @@
+/* -*- mode: C -*- Time-stamp: "2011-02-23 22:20:17 jemarch"
+ *
+ *       File:         pdf-object.h
+ *       Date:         Wed Feb 23 22:18:52 2011
+ *
+ *       GNU PDF Library - Header file for the Object Layer
+ *
+ */
+
+/* Copyright (C) 2011 Free Software Foundation, Inc. */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * The purpose of this file is to provide the symbols exported from
+ * the base modules to other library layers.  By using this header we
+ * can change the structure of modules without impacting files in
+ * other layers.
+ */
+
+#ifndef PDF_OBJECT_H
+#define PDF_OBJECT_H
+
+#include <config.h>
+
+#include <pdf-obj.h>
+
+#endif /* !PDF_OBJECT_H */
+
+/* End of pdf-object.h */
