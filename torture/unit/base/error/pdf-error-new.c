ed76706fddadf6098437613556fb35acfe6d5439
Aleksander Morgado <aleksander@gnu.org>
2011-03-04 23:47:24 +0100

    torture: Unit tests for pdf_error_new() ready

--- torture/unit/base/error/pdf-error-new.c
+++ torture/unit/base/error/pdf-error-new.c
@@ -46,12 +46,53 @@ START_TEST (pdf_error_new_001)
   pdf_init ();
 
   error = pdf_error_new (PDF_EDOMAIN_BASE_BASIC,
-			 PDF_EAGAIN,
-			 format);
+                         PDF_EAGAIN,
+                         format);
   fail_if (!error);
   fail_if (pdf_error_get_status (error) != PDF_EAGAIN);
   fail_if (pdf_error_get_domain (error) != PDF_EDOMAIN_BASE_BASIC);
   fail_if (strcmp (pdf_error_get_message (error), format) != 0);
+
+  pdf_error_destroy (error);
+}
+END_TEST
+
+/*
+ * Test: pdf_error_new_002
+ * Description:
+ *   Create a pdf_error_t with a printf-like message format and arguments.
+ * Success condition:
+ *   The pdf_error_t is created, and its contens are the expected
+ *   ones.
+ */
+START_TEST (pdf_error_new_002)
+{
+  pdf_error_t *error;
+
+  const pdf_char_t *format =
+    "This is a message with a string: %s and "
+    "a number: %d, and another unsigned long number: %lu";
+
+  const pdf_char_t *string = "STRING";
+  int number = -1234;
+  unsigned long ulong = 123456789;
+
+  const pdf_char_t *expected =
+    "This is a message with a string: STRING and "
+    "a number: -1234, and another unsigned long number: 123456789";
+
+  pdf_init ();
+
+  error = pdf_error_new (PDF_EDOMAIN_BASE_BASIC,
+                         PDF_EAGAIN,
+                         format,
+                         string,
+                         number,
+                         ulong);
+  fail_if (!error);
+  fail_if (pdf_error_get_status (error) != PDF_EAGAIN);
+  fail_if (pdf_error_get_domain (error) != PDF_EDOMAIN_BASE_BASIC);
+  fail_if (strcmp (pdf_error_get_message (error), expected) != 0);
 
   pdf_error_destroy (error);
 }
@@ -68,6 +109,7 @@ test_pdf_error_new (void)
   tc = tcase_create("pdf_error_new");
 
   tcase_add_test(tc, pdf_error_new_001);
+  tcase_add_test(tc, pdf_error_new_002);
   return tc;
 }
 
